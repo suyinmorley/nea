65b87b2d6ede5133205902981c0f5600e86766cf
suyin <lon25morl01@mpw.ac.uk>
2026-03-09 14:56:22 +0000

    Add signup route and link to signup page in index.html

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/signup")
+def signup():
+    return"signup page"
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
