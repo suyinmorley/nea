d526cb5b064da4359face1fcf296d973426c0377
suyin <lon25morl01@mpw.ac.uk>
2026-03-16 11:26:13 +0000

    Implement signup functionality and create signup page

--- app.py
+++ app.py
@@ -4,15 +4,23 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return"signup page"
+    if request.method == "POST":
+        f = open("users.txt", "w")
+        f.write(request.form['pw'])
+        f.close()
+    return render_template("signup.html")
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         pw = request.form['pw']
-        if pw == "1234":
+        f = open("users.txt", "r")
+        stored_pw = f.read()
+        f.close()
+        if pw == stored_pw:
             return "Correct password!"
         else:
             return "Incorrect password!"
